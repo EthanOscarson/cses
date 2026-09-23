91b7265a313f4fd2b6c24e99a34750f122fe4b6e
Ethan Oscarson <morrowoscarson@gmail.com>
2026-09-22 22:07:18 -0700

    basic comments and formatting

--- Missing Number/main.c
+++ Missing Number/main.c
@@ -3,14 +3,18 @@
 int main(void) {
     long long n = 0;
     scanf("%lld", &n);
-    long long terminalN = (n * (n+1))/2;
+    
+    long long terminalN = (n * (n+1))/2; // formula for terminal, where 5? = 5+4+3+2+1
     long long sum = 0;
+
+    // sum all numbers inputted
     for(int i = 0; i < n-1; i++) {
         long long inputNumber = 0;
         scanf("%lld", &inputNumber);
         sum += inputNumber;
     }
-    long long missing = terminalN - sum;
+    
+    long long missing = terminalN - sum; // find missing by subtracting the terminal from the actual sum
     printf("%lld", missing);
 }
 
--- Weird Algorithm/main.c
+++ Weird Algorithm/main.c
@@ -4,15 +4,19 @@
 int main() {
     long long a;
     scanf("%lld", &a);
+    
     while (true) {
         printf("%lld ", a);
+        
         if(a==1) {
-            break;
+            break; // run until it equals 1
         }
+        
         if(a%2==0) {
-            a = a/2;
+            a = a/2; // if even, divide by two
+
         } else {
-            a = (a*3) + 1;
+            a = (a*3) + 1; // if odd, multiply by 3 and add 1
         }
         
     }
